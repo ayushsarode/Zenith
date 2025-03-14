ca7c8c06305ec7a598daaadcd008ac2ed7b88608
ayushsarode <ayushsarode777@gmail.com>
2025-03-14 16:15:33 +0530

    folder structure changed

--- main.go
+++ main.go
@@ -1,18 +1,1 @@
 package main
-
-import (
-	"fmt"
-	"os"
-
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-)
-
-var cfgFile string
-
-func main() {
-	if err := rootCmd.Execute(); err!= nil {
-		fmt.Println(err);
-		os.Exit(1)
-	}
-}
